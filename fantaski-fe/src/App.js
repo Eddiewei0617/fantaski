c66bdc0316a6d3891493880e1b568ca6ae5956d2
tunahua <leejunkilowinnive@gmail.com>
2021-11-09 15:58:17 +0800

    1109 add navbar done

--- fantaski-fe/src/App.js
+++ fantaski-fe/src/App.js
@@ -1,5 +1,7 @@
 // import "./App.css";
 import Navbar from "./components/share/Navbar";
+import Footer from "./components/share/Footer";
+import Gotop from "./components/share/Gotop";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 // 引入各分頁(後續寫程式可更動)
@@ -11,13 +13,40 @@ import Forum from "./pages/Forum";
 import Order from "./pages/Order";
 import Login from "./pages/Login";
 
+// footer 相關連結
+import Aboutus from "./pages/footer/Aboutus";
+import Joinus from "./pages/footer/Joinus";
+import Terms from "./pages/footer/Terms";
+import Servicepolicy from "./pages/footer/Servicepolicy";
+import Privacy from "./pages/footer/Privacy";
+
 function App() {
   return (
     <Router>
       <>
         <Navbar />
-        {/* switch 最後在所有網頁切版完在設定 */}
+        <Gotop />
         <Switch>
+          {/* footer */}
+          {/* 有時間在處理 */}
+          {/* <Route path="/privacy" exact>
+            <Privacy />
+          </Route>
+          <Route path="/servicepolicy" exact>
+            <Servicepolicy />
+          </Route>
+          <Route path="/terms" exact>
+            <Terms />
+          </Route>
+          <Route path="/joinus" exact>
+            <Joinus />
+          </Route>
+          <Route path="/aboutus" exact>
+            <Aboutus />
+          </Route> */}
+
+          {/* switch 最後在所有網頁切版完在設定 */}
+
           <Route path="/login">
             <Login />
           </Route>
@@ -40,6 +69,7 @@ function App() {
             <Home />
           </Route>
         </Switch>
+        <Footer />
       </>
     </Router>
   );
