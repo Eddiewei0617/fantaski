84f1acead92b9b061120babebae9b8169c0710c4
jessiechen <jessie82923@gmail.com>
2021-11-17 21:46:07 +0800

    1117 merge main & 調整alert順序

--- fantaski-fe/src/components/course/addCartFix/AddCartFix.js
+++ fantaski-fe/src/components/course/addCartFix/AddCartFix.js
@@ -137,7 +137,10 @@ function AddCartFix({
             <button
               className={ifAddCart ? "button-clicked" : ""}
               onClick={(e) => {
-                if (customerChoose.date === "") {
+                let itemId = `c-${courseInfo[0].id}`;
+                if (storage[itemId]) {
+                  alert("您已將此物品加入購物車");
+                } else if (customerChoose.date === "") {
                   alert("請填寫日期！");
                 } else if (customerChoose.number > customerChoose.courseLeft) {
                   alert("人數超過上限");
@@ -146,17 +149,13 @@ function AddCartFix({
                   return;
                 } else {
                   setIfAddCart(true);
-                  let itemId = `c-${courseInfo[0].id}`;
                   let productInfo = e.currentTarget.children[0].value;
                   // console.log("value", productInfo); //http://localhost:3000/assets/images_product/allblack.jfif|雪板類|暗黑滿點單板|1200
 
                   // 開始把點"加到購物車"的商品存入storage
-                  if (storage[itemId]) {
-                    alert("您已將此物品加入購物車");
-                  } else {
-                    storage.setItem(itemId, productInfo);
-                    storage["addItemList"] += `${itemId}, `;
-                  }
+
+                  storage.setItem(itemId, productInfo);
+                  storage["addItemList"] += `${itemId}, `;
                 }
               }}
             >
